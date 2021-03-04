2a76c70525ba2e0cf98af36c30a8ad1dd630c29a
Katrina Yancey <katrina@dtlconsult.com>
2021-03-04 10:29:31 -0500

    Add plugin for hosting

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ const descriptionStyle = {
 const docLink = {
   text: "Documentation",
   url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+  color: "#000000",
 }
 
 const badgeStyle = {
